e765f50fe097441e158822f60b6c9e61320c052c
Aashir <aashir.razzaq@teraception.com>
2026-08-06 11:16:21 +0500

    feat: replace vim-tmux-navigator with herdr-splits.nvim
    - replaced vim-tmux-navigator with herdr-splits.nvim as I have moved
    from tmux to herdr

--- init.lua
+++ init.lua
@@ -3,3 +3,6 @@ require("config.keymap")
 require("config.lazy")
 require("config.settings")
 require("config.filetype")
+
+local sock = vim.fn.expand("~/.cache/nvim-locator.sock")
+pcall(vim.fn.serverstart, sock)
